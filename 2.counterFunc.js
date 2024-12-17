6ce6fcf9b1d6ebb62b5ffbeefde3f190d04c881a
GunjalArpita <gunjalarpita80@gmail.com>
2024-12-17 16:35:16 +0530

    Initial commit for Leetcode javaScript Challenge

---
+++ 2.counterFunc.js
@@ -0,0 +1,41 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1
+// more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+// Example 1:
+
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+
+
+
+
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+var createCounter = function(n) {
+    
+    return function() {
+      let c=0
+      while(c<4)
+      {
+         return n++; 
+         } 
+    };
+};
+ let counter=createCounter(10);
+ counter();
+ counter();
+ counter();
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
