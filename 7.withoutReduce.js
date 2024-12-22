b2feb260afa1cf6435e6cf1fc8243e995d1ea97c
GunjalArpita <gunjalarpita80@gmail.com>
2024-12-22 20:26:40 +0530

    Day 7: solved Array problem without using built-in function 'reduce'

---
+++ 7.withoutReduce.js
@@ -0,0 +1,26 @@
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing 
+// the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ...
+// until every element in the array has been processed. The ultimate value of val is then returned.
+// If the length of the array is 0, the function should return init.
+// Example 1:
+// Input:  nums = [1,2,3,4] ,fn = function sum(accum, curr) { return accum + curr; }, init = 0
+// Output: 10, Explanation: , initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10, The final answer is 10.
+
+var reduce = function(nums, fn, init) 
+{
+let result=init;
+for(const num of nums)
+{
+    result=fn(result,num);
+}
+return result;
+}
+nums=[1,2,3,4];
+fn=(accum,curr)=>accum+curr;
+init=0;
+console.log(reduce(nums,fn,init));
